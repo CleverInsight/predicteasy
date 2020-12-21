692c9a9a0bb130e087d2b9e2ce1e251243632a6a
Bastin Robins .J <bastinrobins@gmail.com>
2020-12-21 15:26:10 +0530

    Added: Regressor into supervised core

--- predicteasy/core/supervised/__init__.py
+++ predicteasy/core/supervised/__init__.py
@@ -3,11 +3,12 @@ import pandas as pd
 import numpy as np
 import sklearn
 from joblib import dump, load
-from catboost import CatBoostClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from catboost import CatBoostClassifier, CatBoostRegressor
+
 
 
 
@@ -72,11 +73,105 @@ class Classifier:
 
             all_columns = list(numerical_columns) + list(onehot_columns)
 
-            df_from_array_pipeline = pd.DataFrame(X_values, columns=all_columns)
+            df_from_etree_pipeline = pd.DataFrame(X_values, columns=all_columns)
 
             # feature_importance = pd.Series(
             #     data=etree.named_steps['classifier'].feature_importances_, \
             #     index = np.array(all_columns))
+
+            feature_importance = pd.DataFrame({
+                    'Feature': np.array(all_columns),
+                    'Score': etree.named_steps['classifier'].feature_importances_
+            }).sort_values(by=['Score'], ascending=False)
+
+            self.time_taken[name] = elapsed_time
+            self.result[name] = feature_importance
+            self.cross_score[name] = sklearn.model_selection.cross_val_score(
+                etree, self.X, self.y, cv=4).mean()
+        
+        if (rtype=='dataframe'):
+            return [{"elapsed": self.time_taken[name],
+            "name": name,
+            "cross_val": self.cross_score[name], 
+            'model': result} for name, result in self.result.items()]
+        elif (rtype=='dict'):
+            return [{"elapsed": self.time_taken[name], 
+            "name": name,
+            "cross_val": self.cross_score[name], 
+            'model': result.to_dict(orient='records')} for name, result in self.result]
+        if (rtype=='json'):
+            return [{"elapsed": self.time_taken[name], 
+            "name": name,
+            "cross_val": self.cross_score[name], 
+            'model': result.to_json(orient='records')} for name, result in self.result.items()]
+
+
+
+class Regressor:
+    """
+    A Supervised Regressor is a meta classifier which is used to 
+    evalute the given the data.
+
+    """
+
+    def __init__(self, X, y):
+        self.X = X
+        self.y = y
+        self.scores = []
+        self.features = []
+        self.result = {}
+        self.cross_score = {}
+        self.time_taken = {}
+
+    def scoring(self, rtype='dataframe', multiple=False):
+        if multiple==True:
+            models = [
+                ('extra_tree', sklearn.ensemble.ExtraTreesRegressor()),
+                ('dtree', sklearn.tree.DecisionTreeRegressor()),
+                ('rfc', sklearn.ensemble.RandomForestRegressor()),
+                ('catboost', CatBoostRegressor())
+            ]
+        else:
+            models = [
+                ('extra_tree', sklearn.ensemble.ExtraTreesRegressor())
+            ]
+        
+        numeric_transformer = Pipeline(steps=[
+            ('imputer', SimpleImputer(strategy='median')),
+            ('scaler', StandardScaler())])
+        categorical_transformer = Pipeline(steps=[
+            ('imputer', SimpleImputer(strategy='constant', fill_value='missing')),
+            ('onehot', OneHotEncoder(categories='auto', sparse=False, 
+            handle_unknown='ignore'))])
+        
+        preprocessor = ColumnTransformer(
+            transformers=[
+                ('num', numeric_transformer, self.X.get_numerical()),
+                ('cat', categorical_transformer, self.X.get_categorical())   
+            ],
+            remainder='passthrough', verbose=True
+        )
+        # evaluate on multiple models
+        for name, model  in models:
+            start_time = time.time()
+            etree = Pipeline(steps=[('preprocessor', preprocessor),
+                      ('classifier', model)])
+            etree.fit(self.X, self.y)
+            dump(etree, name+'.joblib')
+            end_time = time.time()
+            elapsed_time = (end_time - start_time) 
+
+            numerical_columns = self.X.get_numerical()
+            onehot_columns = etree.named_steps['preprocessor']\
+                .named_transformers_['cat'].named_steps['onehot']\
+                .get_feature_names(input_features=self.X.get_categorical())
+
+            #you can get the values transformed with your pipeline
+            X_values = preprocessor.fit_transform(self.X)
+
+            all_columns = list(numerical_columns) + list(onehot_columns)
+
+            df_from_etree_pipeline = pd.DataFrame(X_values, columns=all_columns)
 
             feature_importance = pd.DataFrame({
                     'Feature': np.array(all_columns),
--- tests/test_modules.py
+++ tests/test_modules.py
@@ -6,25 +6,26 @@ from sklearn.metrics import confusion_matrix
 from prettytable import PrettyTable
 from predicteasy.core import metrics
 from predicteasy.core.data import TableFrame
-from predicteasy.core.supervised import Classifier
+from predicteasy.core.supervised import Classifier, Regressor
 
 
 def run_model_scoring():
     df = TableFrame(pd.read_csv('data/train.csv'))
-    X = df[['Sex','PassengerId', 'Pclass', 'Ticket', 'Embarked', 'Age', 'SibSp', 'Parch', 'Fare']]
     y = df['Survived']
+    X = df.drop('Survived', axis=1)
     clf = Classifier(X, y)
     return clf.scoring(multiple=False)
 
-models = run_model_scoring()
-x = PrettyTable()
-x.field_names = ["ID", "Model", "Score", "Elapsed"]
-for (index, model) in enumerate(models):
-    x.add_row([index+1, model['name'], model['elapsed'], model['cross_val']])
-print(x)
+
+def run_regressor_model():
+    df = TableFrame(pd.read_csv('data/train.csv'))
+    X = df[['Sex','PassengerId', 'Pclass', 'Ticket', 'Embarked', 'Survived', 'SibSp', 'Parch', 'Fare']]
+    y = df['Age'].fillna(df['Age'].median())
+    clf = Regressor(X, y)
+    return clf.scoring(multiple=True)
 
 
-def test_trained_model():
+def test_trained_clf_model():
     df = TableFrame(pd.read_csv('data/train.csv'))
     X = df[['Sex','PassengerId', 'Pclass', 'Ticket', 'Embarked', 'Age', 'SibSp', 'Parch', 'Fare']].head(20)
     y = df['Survived'].head(20)
@@ -34,4 +35,19 @@ def test_trained_model():
     print(confusion_matrix(predicted, y))
 
 
-test_trained_model()
+
+
+    
+    # clf = load('extra_tree.joblib')
+    # predicted = clf.predict(X)
+    # print(accuracy_score(predicted, y))
+    # print(confusion_matrix(predicted, y))
+
+
+# test_trained_clf_model()
+models = run_model_scoring()
+x = PrettyTable()
+x.field_names = ["ID", "Model", "Score", "Elapsed"]
+for (index, model) in enumerate(models):
+    x.add_row([index+1, model['name'], model['elapsed'], model['cross_val']])
+print(x)
